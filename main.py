05d25b5bee8fb507f45d44668efd3d6721352438
Andrew Ault <andrew@baxter.tamz.us>
2018-03-28 19:32:15 -0400

    Changed name of program inside of the code
    from "bio" to "format"

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-''' Steam Bio Tool By Andrew Ault
+''' Steam Format Tool By Andrew Ault
 3/27/18
 Easily and quickly format your Steam Bio and info box!
 '''
 tool = 0
 checkinput = True
-print("Welcome to the Steam Bio Tool v1.1")
+print("Welcome to the Steam Format Tool v1.1")
 print("Press 'Enter' after each line of text appears to read the directions")
 input()
 print("Options for formatting: Header, Bold, Underline, Italic, Strike, Spoiler, URL")
