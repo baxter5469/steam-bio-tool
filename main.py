b4451a8f22b0daef4d27b2fe3c9d273744b71d83
baxter5469 <33003977+baxter5469@users.noreply.github.com>
2018-03-27 21:20:26 -0400

    first steps for if statements

---
+++ main.py
@@ -0,0 +1,20 @@
+''' Steam Bio Tool By Andrew Ault
+3/27/18
+Easily and quickly format your Steam Bio and info box!
+'''
+checkinput = False
+print("Welcome to the Steam Bio Tool v0.1")
+input()
+print("Press 'Enter' after each line of text appears to read the directions")
+input()
+print("Options for formatting: Header, Bold, Underline, Italic, Strike, spoiler, URL")
+input()
+tool = input("Enter Which format tool you would like to use:")
+if tool == "header" or "Header":
+    print("test")
+if tool == "Bold" or "bold":
+    print("confirmed")
+if tool == "Underline" or "underline":
+    print("confirmed")
+if tool == "Italic" or "italic":
+    print("confirmed")
