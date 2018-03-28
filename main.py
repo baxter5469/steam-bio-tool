89db4e6a98d42ee3dd77373c037926769a359153
baxter5469 <33003977+baxter5469@users.noreply.github.com>
2018-03-28 11:29:02 -0400

    added change str to lowercase
    .lower()

--- main.py
+++ main.py
@@ -11,22 +11,21 @@ input()
 print("Options for formatting: Header, Bold, Underline, Italic, Strike, Spoiler, URL")
 input()
 while checkinput:
-    tool = input(str("Enter Which format tool you would like to use:"))
-    if tool == "header" or tool == "Header":
+    tool = input(str("Enter Which format tool you would like to use:")).lower()
+    if tool == "header"
         checkinput = False
 checkinput = True
-if tool == "header" or tool == "Header":
+if tool == "header":
     print("works :D")
-    '''if tool == "Bold" or "bold":
-        print("confirmed")
-    if tool == "Underline" or "underline":
-        print("confirmed")
-    if tool == "Italic" or "italic":
-        print("confirmed")
-    if tool == "Strike" or "strike":
-        print("confirmed")
-    if tool == "Spoiler" or "spoiler":
-        print("confirmed")
-    if tool == "URL" or "url":
-        print("confirmed")
-'''
+if tool == "Bold":
+    print("confirmed")
+if tool == "Underline":
+    print("confirmed")
+if tool == "Italic":
+    print("confirmed")
+if tool == "Strike":
+    print("confirmed")
+if tool == "Spoiler":
+    print("confirmed")
+if tool == "URL":
+    print("confirmed")
