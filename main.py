4b36fdc0c94aab99ba76f5c9c7b56024c54c7e1a
baxter5469 <33003977+baxter5469@users.noreply.github.com>
2018-03-27 21:52:17 -0400

    fixed input valididation
    forgot to add after the or "tool =="

--- main.py
+++ main.py
@@ -2,19 +2,31 @@
 3/27/18
 Easily and quickly format your Steam Bio and info box!
 '''
-checkinput = False
+tool = 0
+checkinput = True
 print("Welcome to the Steam Bio Tool v0.1")
 input()
 print("Press 'Enter' after each line of text appears to read the directions")
 input()
-print("Options for formatting: Header, Bold, Underline, Italic, Strike, spoiler, URL")
+print("Options for formatting: Header, Bold, Underline, Italic, Strike, Spoiler, URL")
 input()
-tool = input("Enter Which format tool you would like to use:")
-if tool == "header" or "Header":
-    print("test")
-if tool == "Bold" or "bold":
-    print("confirmed")
-if tool == "Underline" or "underline":
-    print("confirmed")
-if tool == "Italic" or "italic":
-    print("confirmed")
+while checkinput:
+    tool = input(str("Enter Which format tool you would like to use:"))
+    if tool == "header" or tool == "Header":
+        checkinput = False
+checkinput = True
+if tool == "header" or tool == "Header":
+    print("works :D")
+    '''if tool == "Bold" or "bold":
+        print("confirmed")
+    if tool == "Underline" or "underline":
+        print("confirmed")
+    if tool == "Italic" or "italic":
+        print("confirmed")
+    if tool == "Strike" or "strike":
+        print("confirmed")
+    if tool == "Spoiler" or "spoiler":
+        print("confirmed")
+    if tool == "URL" or "url":
+        print("confirmed")
+'''
