dcca52029a40d30ebf1f2052d9b7205f7b065aa9
Andrew Ault <andrew@baxter.tamz.us>
2018-03-28 21:16:56 -0400

    updated verison number

--- Source Code/main.py
+++ Source Code/main.py
@@ -1,10 +1,10 @@
-''' Steam Format Tool By Andrew Ault
+''' Steam Format Tool By Andrew Ault v1.2
 3/27/18
 Easily and quickly format your Steam Bio and info box!
 '''
 tool = 0
 checkinput = True
-print("Welcome to the Steam Format Tool v1.1")
+print("Welcome to the Steam Format Tool v1.2")
 print("Press 'Enter' after each line of text appears to read the directions")
 input()
 print("Options for formatting: Header, Bold, Underline, Italic, Strike, Spoiler, URL")
